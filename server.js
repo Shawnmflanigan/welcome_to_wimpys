4d40f7916cd5d4d923d2c5abe26c45aa1c9d067b
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-02-15 09:59:05 -0800

    add basic boilerplate for handlebars and server, update pAckage.json

---
+++ server.js
@@ -0,0 +1,27 @@
+// dependancies
+const express = require('express');
+const exphbs = require('express-handlebars');
+const mysql = require('mysql');
+
+const app = express();
+
+// set up server
+const PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+// Start our server so that it can begin listening to client requests.
+// Log (server-side) when our server has started
+app.listen(PORT, () =>
+  console.log(`Server listening on: http://localhost:${PORT}`)
+);
