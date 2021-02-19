61014f9b54a1d0bd0b1c470b9d41a96eff4f6e48
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-02-19 15:51:27 -0800

    further testing

--- config/connection.js
+++ config/connection.js
@@ -1,28 +1,22 @@
-// Dependancies
+const util = require('util');
 const mysql = require('mysql');
 
-// mySql connection
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "B00tC@mp",
-    database: "burgers_db"
-});
-
-// Connect to the DB
-connection.connect((err) => {
-    if (err) {
-      console.error(`error connecting: ${err.stack}`);
-      return;
-    }
-    console.log(`connected as id ${connection.threadId}`);
-});
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'B00tC@mp',
+        database: 'burgers_db',
+    })
 };
-    
+connection.connect((err) => {
+    if (err) throw err;
+    console.log(`Connected as id ${connection.threadId} \n`);
+});
 
-// Export module
+connection.query = util.promisify(connection.query);
+
 module.exports = connection;
