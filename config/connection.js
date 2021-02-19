971f4ec924a7d447587344f401442c9f588d335e
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-02-19 15:47:10 -0800

    bug testing heroku deploy.

--- config/connection.js
+++ config/connection.js
@@ -1,29 +1,28 @@
 // Dependancies
 const mysql = require('mysql');
-const connection;
 
 // mySql connection
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection({
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "B00tC@mp",
     database: "burgers_db"
-  });
-}
-// Connect to the DB
-connection.connect((err) => {
-  if (err) {
-    console.error(`error connecting: ${err.stack}`);
-    return;
-  }
-  console.log(`connected as id ${connection.threadId}`);
 });
 
+// Connect to the DB
+connection.connect((err) => {
+    if (err) {
+      console.error(`error connecting: ${err.stack}`);
+      return;
+    }
+    console.log(`connected as id ${connection.threadId}`);
+});
+};
+    
 
 // Export module
-connection.connect();
 module.exports = connection;
