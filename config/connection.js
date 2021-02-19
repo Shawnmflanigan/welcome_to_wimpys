5bf423e1ba7efb59efade2ae98329ede5f1b7ce1
Shawn Flanigan <shawnmflanigan@gmail.com>
2021-02-19 15:35:03 -0800

    add config changes for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,24 +1,29 @@
 // Dependancies
 const mysql = require('mysql');
+const connection;
 
 // mySql connection
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "B00tC@mp",
     database: "burgers_db"
-});
-
+  });
+}
 // Connect to the DB
 connection.connect((err) => {
-    if (err) {
-      console.error(`error connecting: ${err.stack}`);
-      return;
-    }
-    console.log(`connected as id ${connection.threadId}`);
+  if (err) {
+    console.error(`error connecting: ${err.stack}`);
+    return;
+  }
+  console.log(`connected as id ${connection.threadId}`);
 });
-    
+
 
 // Export module
+connection.connect();
 module.exports = connection;
